afaec8f191af79ad5c18bbc33dea4ea4fc50ff78
Joe Sembler <joeysembler@gmail.com>
2021-12-09 17:15:41 -0500

    Added search bar amongst other things

--- script.js
+++ script.js
@@ -2,40 +2,42 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
+let characterDisplayed = {};
+
 document.addEventListener("DOMContentLoaded", function() {
-
-    
-    // const mainDisplay = document.getElementById("main-display");
-
     fetch("http://hp-api.herokuapp.com/api/characters")
         .then(resp => resp.json())
         .then(data => {
+            // createDbArray();    
             const randomInt = getRandomInt(25);
             const character = data[randomInt];
-            displayCharacter(character);
+            character.id = randomInt;
+            displayCharacter(character);  
         })
 })
 
-
 function displayCharacter(character) {
+
+    changeHouseColors(character.house);
+
     const mainImage = document.getElementById('main-image');
-    
-    
+    const mainList = document.querySelector('#main-display ul'); 
+
     const characterName = document.querySelector('#main-display h3');
     const characterHouse = document.getElementById('house');
     const characterAncestry = document.getElementById('ancestry');
-    const mainList = document.querySelector('#main-display ul');
-
-            
+    const characterDoB = document.getElementById('dob');
+    const characterPatronus = document.getElementById('patronus');
 
     mainImage.src = character.image;
-            
-            
-    characterName.innerHTML = character.name;
-            
-    characterHouse.innerHTML = "House: " + character.house;
-    characterAncestry.innerHTML = "Ancestry: " + character.ancestry;
-    changeHouseColors(character.house);
+                 
+    characterName.innerHTML = character.name;      
+    characterHouse.innerHTML = "House: " + (character.house ? character.house : "n/a");
+    characterAncestry.innerHTML = "Ancestry: " + (character.ancestry ? character.ancestry : "n/a");
+    characterDoB.innerHTML = "Date of Birth: " + (character.dateOfBirth ? character.dateOfBirth : "n/a");
+    characterPatronus.innerHTML = "Patronus: " + (character.patronus ? character.patronus : "n/a");
+    characterDisplayed = character;
+    characterDisplayed.id = character.id;
 }
 
 const nextButton = document.getElementById('next-button');
@@ -53,28 +55,34 @@ nextButton.addEventListener("click", () => {
 function changeHouseColors(house){
 
     const header = document.querySelector("header");
+    const pageTitle = document.querySelector('h2');
     const name = document.querySelector('#main-display h3');
     const list = document.querySelector('#main-display ul');
+    
 
     if(house.toLowerCase() == "gryffindor"){
         header.style.backgroundColor = '#AE0001';
         list.style.backgroundColor = '#EEBA30';
         name.style.backgroundColor = '#EEBA30';
+        pageTitle.style.textShadow = '.025em .05em .15em #EEBA30';
     }
     else if(house.toLowerCase() == "slytherin"){
         header.style.backgroundColor = '#2A623D';
         list.style.backgroundColor = '#AAAAAA';
         name.style.backgroundColor = '#AAAAAA';
+        pageTitle.style.textShadow = '.025em .05em .08em #AAAAAA';
     }
     else if(house.toLowerCase() == "ravenclaw"){
         header.style.backgroundColor = '#222F5B';
         list.style.backgroundColor = '#946B2D';
         name.style.backgroundColor = '#946B2D';
+        pageTitle.style.textShadow = '.025em .05em .15em #946B2D';
     }
     else if(house.toLowerCase() == "hufflepuff"){
         header.style.backgroundColor = '#FFDB00';
         list.style.backgroundColor = '#60605C';
         name.style.backgroundColor = '#60605C';
+        pageTitle.style.textShadow = '.025em .05em .45em #000000';
     }
     else {
         header.style.backgroundColor = '#FFFDD0';
@@ -91,12 +99,7 @@ searchButton.addEventListener("submit", (event) => {
     fetch("http://hp-api.herokuapp.com/api/characters")
     .then(resp => resp.json())
     .then(data => {
-        console.log(searchInput.value);
         const character = searchCharacter(data, searchInput.value);
-        console.log(character);
-        
-        // displayCharacter(character);
-        
     })
     // // TRYING TO RESET THE PLACEHOLDER TEXT
     // const nameHeader = document.querySelector('#main-display h3');
@@ -108,19 +111,74 @@ searchButton.addEventListener("submit", (event) => {
 
 function searchCharacter(data, searchInput){
     let characterFound = false;
+    const error = document.getElementById('search-error');
     data.forEach(character => {
         if(character.name.toLowerCase() == searchInput.toLowerCase()){
-            console.log("Character Found!");
             characterFound = true;
             displayCharacter(character);
-            console.log(character);
+            error.innerHTML = "";
             return character;
             
         }
     });
     if(characterFound == false){
         console.log("no character found");
+        error.innerHTML = "No character found. Check your spelling and try again.";
         return null;
     }
 }
 
+
+// // TRYING TO GET LIKES TO PERSIST AND CREATE RANKING SYSTEM
+// const likeButton = document.getElementById('like-button');
+// likeButton.addEventListener('click', function() {
+//     let likeCount;
+//     let likeObj = {
+//         likes: likeCount
+//     };
+//     fetch(`http://localhost:3000/characters/${characterDisplayed.id}`, {
+//         method: 'PATCH',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(likeObj)
+//     })
+//         .then(resp => resp.json())
+// })
+
+// function createDbArray(){
+//     let i = 0;
+//     let array = [];
+//     fetch("http://hp-api.herokuapp.com/api/characters")
+//         .then(resp => resp.json())
+//         .then(data => {
+//             data.forEach(character => {
+//                 let obj = {
+//                     id: i,
+//                     name: character.name,
+//                     image: character.image,
+//                     likes: 0
+//                 }
+//                 i++;
+//                 array.push(obj);
+//             })
+//         })
+    
+    
+//     fetch('http://localhost:3000/characters', {
+//         method: 'POST',
+//         headers: {
+//              'Content-Type': 'application/json'
+//         },
+//             body: JSON.stringify(array)
+//     })
+//     .then(resp => resp.json())
+
+//     fetch('http://localhost:3000/characters')
+//     .then(resp => resp.json())
+//     .then(data => {console.log(data)})
+    
+// }
+    
+
+// Terminal Command = json-server --watch db.json
